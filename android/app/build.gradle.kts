e1aab58f4907b5318c7c2922184f375167bcbd17
政权 李 <1808108819@qq.com>
2025-07-16 22:16:00 +0800

    更换Java版本和ndk版本至正确版本

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -9,10 +9,11 @@ android {
     namespace = "com.example.u_2d_platformergame"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.127077973"
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlinOptions {
